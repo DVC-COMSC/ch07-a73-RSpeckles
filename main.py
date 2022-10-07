ec172dc299d621f4b7df50fe182152fdc7a43259
RSpeckles <rchenspeckman@gmail.com>
2022-10-07 16:38:35 -0700

    Completed assignment

--- main.py
+++ main.py
@@ -3,8 +3,21 @@
 numbers = [5, 20, 30, 35, 50]
 
 insval = int(input('Enter the insertion value: '))
-# ******************************
-# Make your Code
-# ******************************
 
-prinnt (numbers)
+#flag variable that tells if insval has been inserted into the list
+flag = 0
+
+#This loop runs through numbers and sees if insval is less than or equal than the current value
+#This is to find where it should be placed in the order
+#if it is, the program inserts the number, turns flag to 1, and breaks the loop, so that insval is not inserted multiple times
+for i in range(len(numbers)):
+    if insval <= numbers[i]:
+        numbers.insert(i, insval)
+        flag = 1
+        break
+
+if flag == 0:
+    numbers.append(insval)
+
+
+print (numbers)
